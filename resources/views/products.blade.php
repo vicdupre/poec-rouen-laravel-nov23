31037c3cad9c37c97e0098ce874413901be4697b
Victor Dupré <dupre.victor@live.fr>
2023-11-21 16:58:18 +0100

    init

---
+++ resources/views/products.blade.php
@@ -0,0 +1,17 @@
+@extends("layout.app")
+
+@section("content")
+<h1>{{ $name }}</h1>
+@if ($name === "Toto")
+<p>Je suis toto</p>
+@endif
+<x-button title="bonjour" />
+<x-button />
+<x-button />
+<x-button />
+<ol>
+    @foreach ($products as $product )
+    <li>{{$product}}</li>
+    @endforeach
+</ol>
+@endsection
