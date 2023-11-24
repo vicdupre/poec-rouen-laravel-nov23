b727c4f7741e540f1161373e70ab1b47ded341a2
Victor Dupré <dupre.victor@live.fr>
2023-11-24 11:22:15 +0100

    cards ui

--- resources/views/recettes/index.blade.php
+++ resources/views/recettes/index.blade.php
@@ -1,27 +1,36 @@
 <x-app-layout>
-    <div class="container">
-        <h1>Liste des recettes</h1>
-        <a href="{{ route('recettes.create') }}">
-            <button type="button">Ajouter une recette</button>
-        </a>
-        <ul>
-            @foreach ($recettes as $recette )
-            <li>{{ $recette->name }} {{$recette->time_to_prepare}} min | {{ $recette->category->name }} |
-                @foreach ($recette->ingredients as $ingredient)
-                {{ $ingredient->name }},
-                @endforeach
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
 
-                @auth
-                @if (Auth::id() == $recette->user_id)
-                <form action="{{ route('recettes.destroy',$recette->id ) }}" method="post">
-                    @csrf
-                    @method("DELETE")
-                    <button type="submit">Supprimer</button>
-                </form>
-                @endif
-                @endauth
-            </li>
-            @endforeach
-        </ul>
+
+
+                    <h1>Liste des recettes</h1>
+                    <a href="{{ route('recettes.create') }}">
+                        <button type="button">Ajouter une recette</button>
+                    </a>
+                    <ul>
+                        @foreach ($recettes as $recette )
+                        <li>{{ $recette->name }} {{$recette->time_to_prepare}} min | {{ $recette->category->name }} |
+                            @foreach ($recette->ingredients as $ingredient)
+                            {{ $ingredient->name }},
+                            @endforeach
+
+                            @auth
+                            @if (Auth::id() == $recette->user_id)
+                            <form action="{{ route('recettes.destroy',$recette->id ) }}" method="post">
+                                @csrf
+                                @method("DELETE")
+                                <button type="submit">Supprimer</button>
+                            </form>
+                            @endif
+                            @endauth
+                        </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
 </x-app-layout>
