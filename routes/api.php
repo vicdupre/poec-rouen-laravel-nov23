156005bdc033dc167d11568a20154012619ef2a6
Victor Dupré <dupre.victor@live.fr>
2023-11-24 14:42:49 +0100

    ingredients ressource

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\api\RecetteController;
+use App\Http\Controllers\api\IngredientsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,4 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::apiResource("/recettes", RecetteController::class);
+Route::apiResource("/ingredients", IngredientsController::class);
 //Route::apiResource("recettes.ingredients", RecetteController::class);
